e18cc1aa1f6d8b092bfce9745b2ae987f26701cb
olarif <76481740+olarif@users.noreply.github.com>
2021-06-26 14:34:02 +0200

    Revert "AudioManager Singleton"
    This reverts commit 2fd7ad4fe848db36c9efb39343d766602cf97354.

--- The Last Potion/Assets/Scripts/AudioManager.cs
+++ The Last Potion/Assets/Scripts/AudioManager.cs
@@ -5,32 +5,18 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Singleton Instance
-    public static AudioManager Instance { get; private set; }
-
     // FMOD Instances
     private FMOD.Studio.EventInstance IMusic;
 
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
+    // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
         SetupInstances();
         IMusic.start();
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
