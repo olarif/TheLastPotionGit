2fd7ad4fe848db36c9efb39343d766602cf97354
Kappinski <capsplay25@gmail.com>
2021-06-26 11:45:51 +0200

    AudioManager Singleton
    made the Audio Manager a singleton and added it to Both scenes

--- The Last Potion/Assets/Scripts/AudioManager.cs
+++ The Last Potion/Assets/Scripts/AudioManager.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    // Singleton Instance
+    public static AudioManager Instance { get; private set; }
+
     // FMOD Instances
     private FMOD.Studio.EventInstance IMusic;
 
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
         SetupInstances();
         IMusic.start();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
