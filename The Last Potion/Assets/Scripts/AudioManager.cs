4ec6e492d0dc3daf0d37f27ceed38511fadb1402
olarif <76481740+olarif@users.noreply.github.com>
2021-07-01 11:35:22 +0200

    recorder package

--- The Last Potion/Assets/Scripts/AudioManager.cs
+++ The Last Potion/Assets/Scripts/AudioManager.cs
@@ -17,7 +17,7 @@ public class AudioManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(this.gameObject);
         }
         else
         {
