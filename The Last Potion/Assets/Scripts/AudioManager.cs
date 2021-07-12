5fec155a838f9f58ea6cff367a45b147b89ef623
Kappinski <capsplay25@gmail.com>
2021-07-12 03:55:28 +0200

    Audio Manager Update

--- The Last Potion/Assets/Scripts/AudioManager.cs
+++ The Last Potion/Assets/Scripts/AudioManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -9,8 +10,11 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance { get; private set; }
 
     // FMOD Instances
-    private FMOD.Studio.EventInstance IMusic;
+    private FMOD.Studio.EventInstance ISoundtrack;
 
+    // Scenes
+    public String MenuScene;
+    public String GameScene;
 
     private void Awake()
     {
@@ -23,12 +27,13 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        SwitchMusic();
     }
 
     void Start()
     {
-        SetupInstances();
-        IMusic.start();
+        SetupInstance();
     }
 
     void Update()
@@ -39,8 +44,23 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    void SetupInstances()
+    void SetupInstance()
     {
-        IMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Music");
+        ISoundtrack = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Soundtrack");
+        ISoundtrack.start();
+    }
+
+    void SwitchMusic()
+    {
+        if (SceneManager.GetActiveScene().name == MenuScene)
+        {
+            Debug.Log("Menu");
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("SceneName", 0f);
+        }
+        else if (SceneManager.GetActiveScene().name == GameScene)
+        {
+            Debug.Log("Outdoor");
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("SceneName", 1f);
+        }
     }
 }
