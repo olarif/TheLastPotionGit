af9bedf466a20577f99ac74b1d50e84444a40736
olarif <76481740+olarif@users.noreply.github.com>
2021-07-15 00:11:55 +0200

    yeet

--- The Last Potion/Assets/Timelines/Scripts/scene2Trigger.cs
+++ The Last Potion/Assets/Timelines/Scripts/scene2Trigger.cs
@@ -8,19 +8,54 @@ public class scene2Trigger : MonoBehaviour
 {
     public PlayableDirector scene2;
     public GameObject chatBubble;
-
     private bool inRange;
+
+    public GameObject shelfInventory;
+    private Inventory inventory;
+
+    public Item item1;
+    public Item item2;
+    public Item item3;
+    public Item item4;
 
     public void Update()
     {
         if (inRange && Input.GetKeyDown(KeyCode.E))
         {
-            scene2.Play();
+            //shelfInventory.SetActive(true);
             chatBubble.SetActive(false);
-            inRange = false;
+            scene2.Play();
         }
     }
 
+    public void AddItem1()
+    {
+        if (inventory.itemList.Count < 4)
+            inventory.AddItem(item1);
+    }
+
+    public void AddItem2()
+    {
+        if (inventory.itemList.Count < 4)
+            inventory.AddItem(item2);
+    }
+
+    public void AddItem3()
+    {
+        if (inventory.itemList.Count < 4)
+            inventory.AddItem(item3);
+    }
+
+    public void AddItem4()
+    {
+        if (inventory.itemList.Count < 4)
+            inventory.AddItem(item4);
+    }
+
+    public void SetInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -31,5 +66,6 @@ public class scene2Trigger : MonoBehaviour
     {
         inRange = false;
         chatBubble.SetActive(false);
+        shelfInventory.SetActive(false);
     }
 }
